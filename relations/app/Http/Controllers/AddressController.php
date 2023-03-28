450f3b2886134e6b5fde2f619a6264aa681c96c8
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-03-28 02:21:38 -0300

    Estrutura Básica do Projeto 3

--- relations/app/Http/Controllers/AddressController.php
+++ relations/app/Http/Controllers/AddressController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Address;
+
 class AddressController extends Controller
 {
-    //
+    public function index(Request $request) 
+    {
+        $addresses = Address::all();
+        return $addresses;
+    }
 }
